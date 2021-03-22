c56ba0039e3cd3f940afe805543b56352b441f13
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-03-22 17:52:50 +0600

    data update delete ans select

--- node_mysql/index.js
+++ node_mysql/index.js
@@ -14,18 +14,59 @@ con.connect(function(error){
     }
     else {
         console.log("Database Connection Success");
-        insertData(con);
+        //insertData(con);
+        //updateData(con);
+        //deletetData(con);
+        selectData(con);
     }
 });
 
-function insertData(con){
-    let sqlQuery =" INSERT INTO `student_tbl`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Rafiqul Islam','100','Nine','01991166550','Satkhira')";
+// function insertData(con){
+//     let sqlQuery =" INSERT INTO `student_tbl`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Rafiqul Islam','100','Nine','01991166550','Satkhira')";
 
-    con.query(sqlQuery, (error) =>{
+//     con.query(sqlQuery, (error) =>{
+//         if(error){
+//             console.log("Data not inserted");
+//         }else{
+//             console.log("Data inserted successfully");
+//         }
+//     });
+// }
+
+// function updatetData(con){
+//     let sqlQuery =" UPDATE `student_tbl` SET  roll`='101' WHERE 1";
+
+//     con.query(sqlQuery, (error) =>{
+//         if(error){
+//             console.log("Data not inserted");
+//         }else{
+//             console.log("Data inserted successfully");
+//         }
+//     });
+// }
+
+
+// function deleteData(con){
+//     let sqlQuery =" DELETE FROM `student_tbl` where id ='1'";
+
+//     con.query(sqlQuery, (error) =>{
+//         if(error){
+//             console.log("Data not inserted");
+//         }else{
+//             console.log("Data inserted successfully");
+//         }
+//     });
+// }
+
+
+function selectData(con){
+    let sqlQuery ="SELECT * FROM `student_tbl`";
+
+    con.query(sqlQuery, (error, result) =>{
         if(error){
             console.log("Data not inserted");
         }else{
-            console.log("Data inserted successfully");
+            console.log(result);
         }
     });
 }
